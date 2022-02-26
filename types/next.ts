c4a52c2fbc365682bfb3d74d39d9fe8bd47100b5
PINOT <pinot.kim@kakao.com>
2022-02-27 01:54:26 +0900

    Add `NextApiResponseServerIO` type

---
+++ types/next.ts
@@ -0,0 +1,12 @@
+import { NextApiResponse } from "next";
+// nodeJS network api (https://nodejs.org/api/net.html)
+import { Socket, Server as NetServer } from "net";
+import { Server as SocketIOServer } from "socket.io";
+
+export type NextApiResponseServerIO = NextApiResponse & {
+	socket: Socket & {
+		server: NetServer & {
+			io: SocketIOServer;
+		};
+	};
+};
