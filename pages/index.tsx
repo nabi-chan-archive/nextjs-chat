7130e7a7f0baeb7a09d6d83f403c976b2cb8b1c4
PINOT <pinot.kim@kakao.com>
2022-02-28 20:25:16 +0900

    Make markup for `/`

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,63 +1,16 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { FormEvent, useEffect, useState } from "react";
-import { io } from "socket.io-client";
-
-interface IMsg {
-  user: string;
-  msg: string;
-  createdAt: number;
-}
-
-const user = "User_" + String(~~(Math.random() * 1000000));
+import { FormEvent, useRef, useState } from "react";
+import { useRouter } from "next/router";
 
 const Home: NextPage = () => {
-  useEffect(() => {
-    const socket = io("", {
-      path: "/api/socket.io",
-    });
+  const { push } = useRouter();
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [roomId, setRoomId] = useState<string>("");
 
-    socket.on("connect", () => {
-      console.log("successfully connected socket.io server", socket);
-    });
+  async function handleRoomCreate() {}
 
-    socket.on("connect_error", (err) => {
-      console.log("failed to connect socket.io server", err);
-    });
-
-    socket.on("message", (message: IMsg) => {
-      setChat((prev) => [...prev, message]);
-    });
-
-    return () => {
-      socket.disconnect();
-    };
-  }, []);
-
-  const [chat, setChat] = useState<IMsg[]>([]);
-
-  async function handleSubmit(event: FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-    const input = event.currentTarget.content;
-
-    if (!input.value) return;
-
-    const message = {
-      user,
-      msg: input.value,
-    };
-
-    await fetch("/api/chat", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(message),
-    });
-
-    input.value = "";
-    input.focus();
-  }
+  async function handleRoomJoin(event: FormEvent<HTMLFormElement>) {}
 
   return (
     <>
@@ -65,30 +18,20 @@ const Home: NextPage = () => {
         <title>Socket.io connect example</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={"flex flex-col w-full h-screen"}>
-        <div className={"flex-1"}>
-          <div className="container mx-auto p-4">
-            {chat.length ? (
-              chat.map((chat, i) => (
-                <div key={`msg_${i}`} className={"mb-1"}>
-                  <span className={chat.user === user ? "text-red-500" : ""}>
-                    {chat.user === user ? "Me" : chat.user}
-                  </span>
-                  : {chat.msg}
-                </div>
-              ))
-            ) : (
-              <div className={"text-center text-gray-600 text-xl"}>채팅 기록이 없습니다.</div>
-            )}
-          </div>
-        </div>
-        <form onSubmit={handleSubmit} className={"bg-gray-400 sticky bottom-0"}>
-          <div className="container mx-auto flex items-center gap-x-4 p-4 h-20">
-            <input className={"flex-1 h-full px-4"} type="text" name={"content"} placeholder={"write some chat..."} />
-            <button className={"h-full px-4 bg-white"} type={"submit"}>
-              submit
-            </button>
-          </div>
+      <main className={"flex flex-col gap-y-2 p-4 w-full h-screen border-gray-200"}>
+        <button onClick={handleRoomCreate} className={"px-4 py-2 bg-gray-200 rounded-md"}>
+          채팅방 생성하기
+        </button>
+        <div className={"px-4 py-2 border-2 rounded-md"}>최근 생성된 채팅방 ID : {roomId}</div>
+        <form onSubmit={handleRoomJoin} className={"flex gap-2"}>
+          <input
+            ref={inputRef}
+            className={"flex-1 border-2 px-4 rounded-md"}
+            type="text"
+            name={"room_id"}
+            placeholder={"채팅방 ID"}
+          />
+          <button className={"px-4 py-2 bg-gray-200 rounded-md"}>이 채팅방에 접속하기</button>
         </form>
       </main>
     </>
